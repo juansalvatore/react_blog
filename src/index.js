4da2ec2532e630c6a98a5187e5a5f813abcaaf35
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-18 12:46:39 -0300

    Create firsts route called PostIndex

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import App from './components/app'
+// components
 import reducers from './reducers'
+import PostsIndex from './components/post_index'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+        {/* <Route path="/posts/:id" component={PostsShow} />
+        <Route path="/posts/new" component={PostsNew} /> */}
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.querySelector('.container')
 )
