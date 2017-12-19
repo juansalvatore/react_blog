d0f01da149b753873185067a3adb484aa3b6426c
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-19 01:13:21 -0300

    Create navigation Link and import redux-form

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import promise from 'redux-promise'
 
 // components
 import reducers from './reducers'
 import PostsIndex from './components/post_index'
+import PostsNew from './components/posts_new'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
+      <Switch>
+        <Route path="/posts/new" component={PostsNew} />
         <Route path="/" component={PostsIndex} />
-        {/* <Route path="/posts/:id" component={PostsShow} />
-        <Route path="/posts/new" component={PostsNew} /> */}
-      </div>
+        {/* <Route path="/posts/:id" component={PostsShow} />*/}
+      </Switch>
     </BrowserRouter>
   </Provider>,
   document.querySelector('.container')
